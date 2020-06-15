7dbb19016be3c645ed7b8ffa11e3aa385e9921c7
LPF33 <larspefe@gmail.com>
2020-06-15 23:03:27 +0200

    started aboutMe section

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./Main.css";
 import paintVinylClock from "./vinylClockCanvasJS.js";
 
 export default function App() {
     const vinylClock = useRef();
+
+    const [clickAbout, setClickAbout] = useState(["aboutOff","aboutOn"]);
 
     useEffect(() => {
         const vinylClockCanvas = vinylClock.current;
@@ -12,7 +14,23 @@ export default function App() {
 
     return (
         <div id="main">
+            <div id="vinylform"></div>
+            <div id="vinylInnerForm"></div>
             <canvas ref={vinylClock} id="vinyl"></canvas>
+            <div id="portrait">
+                <div className="outerForm flex">
+                    <div className="innerForm flex">
+                        <div id="myPicture"></div>  
+                    </div>                        
+                </div> 
+                <p className="flex">About me</p>
+                <div className="outerFormButton flex">
+                    <div className="innerFormButton flex">
+                        <div className={`aboutButton flex ${clickAbout[0]}`} onClick={()=>{if(clickAbout[0]==="aboutOff"){let changeAbout=[clickAbout[1],clickAbout[0]]; setClickAbout(changeAbout)}}}>ON</div>  
+                        <div className={`aboutButton flex ${clickAbout[1]}`} onClick={()=>{if(clickAbout[1]==="aboutOff"){let changeAbout=[clickAbout[1],clickAbout[0]]; setClickAbout(changeAbout)}}}>OFF</div>
+                    </div>                        
+                </div>                
+            </div>
         </div>
     );
 }
--- src/vinylClockCanvasJS.js
+++ src/vinylClockCanvasJS.js
@@ -13,18 +13,24 @@ export default function paintVinylClock(canvas) {
         canvas.height = window.innerHeight * 0.7;
 
         //inner white circle
-        ctx.beginPath();
-        ctx.fillStyle = "white";
-        ctx.arc(canvas.width / 2 , canvas.height / 2 , canvas.width * 0.16 , 0 , 2 * Math.PI);
-        ctx.fill();
-        ctx.closePath();
+        const innerWhiteCircle = () => {
+            ctx.beginPath();
+            ctx.fillStyle = "white";
+            ctx.arc(canvas.width / 2 , canvas.height / 2 , canvas.width * 0.16 , 0 , 2 * Math.PI);
+            ctx.fill();
+            ctx.closePath();
+        }
+        
+        innerWhiteCircle();
 
         //button
-        ctx.beginPath();
-        ctx.fillStyle = "rgb(228, 205, 205)";
-        ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width * 0.02, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.closePath();
+        const button = () => {
+            ctx.beginPath();
+            ctx.fillStyle = "rgb(228, 205, 205)";
+            ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width * 0.02, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.closePath();
+        }        
 
         //seconds hand 0 seconds
         const nullSeconds = () => {
@@ -43,18 +49,77 @@ export default function paintVinylClock(canvas) {
             ctx.beginPath();
             ctx.strokeStyle = "rgb(66, 60, 60)";
             ctx.lineWidth = 1.7;
-            ctx.arc( canvas.width/2 , canvas.height/2, canvas.width * 0.2 + 3 * i, 0, 2 * Math.PI);
+            ctx.arc( canvas.width/2 , canvas.height/2, canvas.width * 0.21 + 3 * i, 0, 2 * Math.PI);
             ctx.stroke();
             ctx.closePath();
         }
 
-        //paint seconds hand
-        const innerBlackCircle = canvas.width * (0.2-0.16);
+        //change Color of hour hand
+        const changeColor = () => {
+            const randomNum = () => Math.floor(Math.random()*254);
+            return `rgb(${randomNum()},${randomNum()},${randomNum()})`;
+        }
+
+        //paint seconds/minutes/hours hand
+        const hourSize = canvas.width/65;
+        const paintHours = () => {
+            const date = new Date();
+            let hours = date.getHours();
+            ctx.beginPath();
+            ctx.fillStyle = changeColor();
+            ctx.arc(canvas.width / 2 + canvas.width * 0.12 * Math.sin(2*Math.PI * (12-hours)/12 - Math.PI), canvas.width / 2 + canvas.width * 0.12 * Math.cos(2*Math.PI * hours/12 - Math.PI),hourSize,0,2*Math.PI);
+            ctx.fill();
+            ctx.closePath();
+        }
         
+        const paintMinutes = () => {
+            const date = new Date();
+            let minutes = date.getMinutes();
+            if(minutes>0 && minutes <= 30){
+                ctx.beginPath();
+                ctx.fillStyle = "rgb(240, 214, 100)";
+                ctx.strokeStyle = "rgb(240, 214, 100)";
+                ctx.arc(canvas.width/2, canvas.height/2, canvas.width*0.16, 1.5*Math.PI, Math.PI*minutes/30 - 0.5*Math.PI);
+                ctx.moveTo(canvas.width/2,canvas.height/2);
+                ctx.lineTo(canvas.width/2,canvas.height/2-canvas.height*0.16);
+                ctx.lineTo(canvas.width / 2 + canvas.width * 0.16 * Math.sin(Math.PI - 2*Math.PI/60 * minutes),canvas.width / 2 + canvas.width * 0.16 * Math.cos(Math.PI - 2*Math.PI/60 * minutes))
+                ctx.fill();
+                ctx.stroke();
+                ctx.closePath();
+                button();
+                paintHours();
+            } else if(minutes>0){
+                ctx.beginPath();
+                ctx.fillStyle = "rgb(240, 214, 100)";
+                ctx.strokeStyle = "rgb(240, 214, 100)";
+                ctx.arc(canvas.width/2, canvas.height/2, canvas.width*0.16, 1.5*Math.PI, Math.PI*minutes/30 - 0.5*Math.PI);
+                ctx.fill();
+                ctx.stroke();
+                ctx.closePath();
+
+                ctx.beginPath();
+                ctx.fillStyle = "white";
+                ctx.strokeStyle = "white";
+                ctx.moveTo(canvas.width/2,canvas.height/2);
+                ctx.lineTo(canvas.width/2,canvas.height/2-canvas.height*0.16);
+                ctx.lineTo(canvas.width / 2 + canvas.width * 0.16 * Math.sin(Math.PI - 2*Math.PI/60 * minutes),canvas.width / 2 + canvas.width * 0.16 * Math.cos(Math.PI - 2*Math.PI/60 * minutes))
+                ctx.fill();
+                ctx.stroke();
+                ctx.closePath();
+                button();
+                paintHours();
+            } else {                
+                innerWhiteCircle();
+                button();
+                paintHours();
+            }
+        }
+
+        const innerBlackCircle = canvas.width * (0.2-0.16);        
 
         const paintSeconds = () => {   
             const date = new Date();
-            let seconds = date.getSeconds();
+            let seconds = date.getSeconds();            
             if(seconds>0){                
                 ctx.beginPath();
                 ctx.strokeStyle = "white";
@@ -69,14 +134,17 @@ export default function paintVinylClock(canvas) {
                 ctx.beginPath();
                 ctx.strokeStyle = "black";
                 ctx.lineWidth = innerBlackCircle;
-                ctx.arc(canvas.width/2, canvas.height/2,canvas.width*0.16+innerBlackCircle/2,0,2*Math.PI);
+                ctx.arc(canvas.width/2, canvas.height/2,canvas.width*0.165+innerBlackCircle/2,0,2*Math.PI);
                 ctx.stroke();
                 ctx.closePath();
                 nullSeconds();
+                paintMinutes();
                 timeoutId = setTimeout(paintSeconds,1000);
-            }console.log(seconds);            
+            }           
         }
 
+        paintHours();
+        paintMinutes();
         paintSeconds();
     }
 
