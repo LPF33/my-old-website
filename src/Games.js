1f0d8ad50eb1951610b90b0f6fd1ab1a1912dbaa
LPF33 <larspefe@gmail.com>
2020-06-21 11:36:40 +0200

    heroku

--- src/Games.js
+++ src/Games.js
@@ -10,8 +10,6 @@ export default function Games(){
 
     const serverUrl = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8080" : "https://larspefe.herokuapp.com";
 
-    console.log(process.env);
-
     const vinylGames = useRef();
     const dispatch = useDispatch();
     const [showIframe, setShowIframe] = useState(null);
