22b1e9620432937739e7931751d802bf5ccc79da
LPF33 <larspefe@gmail.com>
2020-06-25 11:13:10 +0200

    mobile orientation front side

--- src/MobileOrientation.js
+++ src/MobileOrientation.js
@@ -5,8 +5,16 @@ export default function MobileOrientation(){
 
     return(
         <div id="mobile-orientation">
-            <div>Welcome to my Website!</div>
-            <div>Please, change the orientation of your device!</div>          
+            <div>
+                <div id="device-outer" className="flex">
+                    <div id="device-inner">
+                        <div id="device-touch">                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h1>Welcome to my Website!</h1>
+            <h2>Please, change the orientation of your device!</h2>          
         </div>        
     )
 }
