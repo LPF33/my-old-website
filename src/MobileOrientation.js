e5441b8a90efdcccd62a716a97bba72ebe1430d6
LPF33 <larspefe@gmail.com>
2020-06-25 14:45:10 +0200

    changed orientation picture

--- src/MobileOrientation.js
+++ src/MobileOrientation.js
@@ -7,10 +7,10 @@ export default function MobileOrientation(){
         <div id="mobile-orientation">
             <div>
                 <div id="device-outer" className="flex">
-                    <div id="device-inner">
-                        <div id="device-touch">                            
-                        </div>
-                    </div>
+                    <div id="device-inner" >
+                        <img src="/website.png" alt="nextButton" />
+                    </div>                    
+                    <div id="device-touch"></div>
                 </div>
             </div>
             <h1>Welcome to my Website!</h1>
