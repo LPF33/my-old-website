e0f1dede6e616da8bac458265756b647f824accb
LPF33 <larspefe@gmail.com>
2020-07-01 18:54:31 +0200

    rabbit tracker game

---
+++ public/games/RabbitTracker/game.js
@@ -0,0 +1,195 @@
+(function(){
+    const grid = $("#grid");
+    const scoreField = $('#score');
+    const timeField = $("#time");
+    const beginner = $("#beginner");
+    const advanced = $("#advanced");
+    const master = $("#master");
+    const gameOverDiv = $("#gameOver");
+
+    class Game{
+        constructor(rabbit, rows, columns){
+            this.rabbits = rabbit;
+            this.gameRows = rows;
+            this.gameColumns = columns;  
+            this.timeoutID = "";    
+        }
+
+        paintGrid(){
+            for(let i=0; i<this.gameRows; i++){
+                const $row = $("<div>").addClass("row");
+                for(let j=0; j<this.gameColumns; j++){
+                    const $column = $("<div>")
+                        .addClass("column hidden")   
+                        .attr("data-row",i)
+                        .attr("data-column",j)             
+                    $row.append($column);
+                }
+                grid.append($row);
+            }
+            scoreField.text(this.rabbits);
+            this.randomRabbit();
+        }
+
+        startTime(seconds=0){       
+            timeField.text(seconds);
+            this.timeoutID = setTimeout(() => this.startTime(++seconds),1000);
+        }
+
+        randomRabbit(){
+            let arr = []
+            while(arr.length<=this.rabbits){
+                const randomCell = Math.floor(Math.random()*(this.gameRows*this.gameColumns));
+                arr.push(randomCell);
+                arr.filter((item,index)=> arr.indexOf(item)===index);
+            }            
+            arr.forEach(item => $(".column.hidden").eq(item).addClass("rabbit"));
+        }        
+
+        detectField(row,column){
+
+            const alreadySearched = {};
+
+            const search = (row,column) => {
+                if(row >=this.gameRows || column >= this.gameColumns || row < 0 || column < 0){
+                    return
+                }               
+                if(alreadySearched[`${row}${column}`]){
+                    return;
+                }else{
+                    alreadySearched[`${row}${column}`] = 1;
+                }
+                const cell = $(`.column.hidden[data-row=${row}][data-column=${column}]`);
+                if(!cell.hasClass("hidden") || cell.hasClass("rabbit")){
+                    return;
+                }
+                cell.removeClass("hidden").removeClass("cage");
+                const rabbitCount = this.countRabbits(row,column);
+                if(rabbitCount){
+                    cell.text(rabbitCount);
+                    return;
+                }
+
+                for(let i=-1; i<=1; i++){
+                    for(let j=-1; j<=1; j++){               
+                        search(row+i,column+j);
+                    }
+                }
+            }
+            search(row,column);       
+            const calc = this.rabbits-[...$(".cage")].length;
+            scoreField.text(calc);      
+        }
+
+        countRabbits(r,c){
+            let counter =0;
+            for(let i=-1; i<=1; i++){
+                for(let j=-1; j<=1; j++){               
+                    const cell = $(`.column.hidden[data-row=${r+i}][data-column=${c+j}]`)
+                    if(cell && cell.hasClass("rabbit") ){
+                        counter++;
+                    }
+                }
+            }            
+            return counter === 0 ? "" : counter;
+        }
+
+        checkWin(){
+            if($(".column.hidden").length === $(".column.hidden.rabbit").length){
+                return true
+            }else{
+                return false;
+            }
+        }
+
+        gameOver(value){
+            clearTimeout(this.timeoutID);
+            value ? 
+                $(".column.hidden.rabbit")
+                    .addClass("cage")
+                    .removeClass("hidden")
+
+                    : 
+                        [...$(".column.hidden")]
+                            .forEach(i =>{
+                                const item = $(i);
+                                if(item.hasClass("rabbit")){
+                                    item.addClass("lost").append("<div class=\"lostAnimation\"></div>");
+                                    item.removeClass("hidden");
+                                    item.removeClass("cage");
+                                }else{
+                                    const row = item.data("row");
+                                    const column = item.data("column");
+                                    const rabbitCount = this.countRabbits(row,column);
+                                    item.text(rabbitCount);
+                                    item.removeClass("hidden")
+                                    item.removeClass("cage");
+                                }
+                            })
+            value ?
+                gameOverDiv
+                    .css("display" , "block")
+                    .text("You win!")
+                    :
+                        gameOverDiv
+                        .css("display" , "block")
+                        .text("You lose!")
+        }
+
+        checkField(item){
+            const row = $(item).data("row");
+            const column = $(item).data("column");
+            if($(item).hasClass("rabbit")){
+                this.gameOver(false);
+            }else{
+                this.detectField(row,column);
+                if(this.checkWin()){
+                    this.gameOver(true);
+                }
+            }
+        }  
+        
+        setCage(e){
+            if($(e.target).hasClass("hidden")){
+                $(e.target).hasClass("cage") ? $(e.target).removeClass("cage") : $(e.target).addClass("cage");
+            }             
+            const calc = this.rabbits-[...$(".cage")].length;
+            scoreField.text(calc);           
+        }
+
+        play(){
+            this.paintGrid();
+            grid.off("click");
+            grid.off("contextmenu");
+            grid.on("click", ".column.hidden", e => this.checkField(e.currentTarget));
+            grid.on("contextmenu", e => {
+                e.preventDefault();
+                if(e.which === 3){
+                    this.setCage(e);
+                }
+            });
+            this.startTime();
+        }
+    }   
+
+    let startGame = "";
+    const play = (e,quant,row,column) => {
+        if(startGame && startGame.timeoutID){
+            clearTimeout(startGame.timeoutID);
+        }
+        target = e ? $(e.currentTarget) : beginner;
+        gameOverDiv.css("display","none");
+        grid.html("");
+        target.parent().children().removeClass("buttonColor");
+        target.addClass("buttonColor");
+        startGame = new Game(quant,row,column);
+        startGame.play();
+    }
+
+    play("",10,9,9);
+
+    beginner.on("click", (e) => play(e,10,9,9));
+    advanced.on("click", (e) => play(e,40,16,16));
+    master.on("click", (e) => play(e,99,16,30)); 
+
+})();
--- sql/games.sql
+++ sql/games.sql
@@ -9,5 +9,6 @@ CREATE TABLE games (
 
 INSERT INTO games (game,info,picturefile) VALUES ('Sudoku', 'Sudoku is played on a grid of 9 x 9 spaces. Within the rows and columns are 9 squares. Each row, column and square needs to be filled out with the numbers 1-9, without repeating any numbers within the row, column or square. ' ,'/sudoku.PNG');
 INSERT INTO games (game,info,picturefile) VALUES ('Memory', 'Who does not know memory? Find all pairs! You can decide how many pairs (16-50). Either choose pictures or numbers or colors (impossible!).','/Memory.PNG');
+INSERT INTO games (game,info,picturefile) VALUES ('RabbitTracker', 'It is played like the Classic game Minesweeper. Find the fields, where the rabbits are hiding and catch them.','/RabbitTracker.PNG');
 INSERT INTO games (game,info,picturefile) VALUES ('ConnectFour', 'The aim for both players is to make a straight line of four own pieces; the line can be vertical, horizontal or diagonal.','/ConnectFour.PNG');
 INSERT INTO games (game,info,picturefile) VALUES ('Snake', 'Snake is a classic computer game in which a snake moving straight or at right angles through a playing field. The aim of the game is to pick up the randomly appearing fruits and to avoid obstacles, including the snake''s own body.','/Snake.png');
